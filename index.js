184f5f4d82ed5a7bef051c65ed7f4c0f305bd5ad
Carson <carsoncolgateccu@gmail.com>
2021-12-15 12:37:52 -0700

    started my index.js and finished function for prompts

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ const Employee = require("./lib/Employee")
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 const Manager = require("./lib/Manager")
+const { generateKey } = require("crypto")
+const employeearray =[];
+promptManager()
+
+function promptStart(){
+    return inqurier.prompt([
+        {
+            type: "list",
+            name: "job",
+            choices:["Engineer", "Intern", "Finish Building team."]
+        },
+    ])
+    .then((answers) =>{
+        if (answers.job === "Engineer"){
+            promptEngineer();
+        } else if (answers.job === "Intern"){
+            promptIntern();
+        } else {
+            generateKey.generatehtml(employeearray);
+            console.log("You've Built your team")
+        }
+    })
+};
+
+function
